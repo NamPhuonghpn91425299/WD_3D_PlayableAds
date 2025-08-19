6d439f9b2519196a49430cea34b1c5aa482351a6
NamHp <115439386+NamPhuonghpn91425299@users.noreply.github.com>
2025-08-19 15:18:53 +0700

    update

--- Assets/_Game/Scripts/GamePlay/CameraController.cs
+++ Assets/_Game/Scripts/GamePlay/CameraController.cs
@@ -196,7 +196,7 @@ public class CameraController : Singleton<CameraController>
             else
             {
                 currentFOV = _mainCamera.fieldOfView;
-                if (MathF.Abs(currentFOV - targetFOV) > 0.15f)
+                if (Mathf.Abs(currentFOV - targetFOV) > 0.15f)
                 {
                     currentFOV = Mathf.Lerp(currentFOV, targetFOV, Time.deltaTime * zoomLerpSpeed);
                     _mainCamera.fieldOfView = currentFOV;
